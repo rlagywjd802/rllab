a4cc28b1ff96c043115ddd9d20b720901a12ece1
Hyojeong <rlagywjd802@gmail.com>
2018-04-26 14:31:13 -0700

    change cartpole to swimmer

--- sandbox/rocky/tf/launchers/trpo_cartpole.py
+++ sandbox/rocky/tf/launchers/trpo_cartpole.py
@@ -1,34 +1,44 @@
 from sandbox.rocky.tf.algos.trpo import TRPO
 from rllab.baselines.linear_feature_baseline import LinearFeatureBaseline
-from rllab.envs.box2d.cartpole_env import CartpoleEnv
+from rllab.envs.mujoco.swimmer_env import SwimmerEnv
 from rllab.envs.normalized_env import normalize
-from sandbox.rocky.tf.optimizers.conjugate_gradient_optimizer import ConjugateGradientOptimizer
-from sandbox.rocky.tf.optimizers.conjugate_gradient_optimizer import FiniteDifferenceHvp
 from sandbox.rocky.tf.policies.gaussian_mlp_policy import GaussianMLPPolicy
 from sandbox.rocky.tf.envs.base import TfEnv
 from rllab.misc.instrument import stub, run_experiment_lite
 
-env = TfEnv(normalize(CartpoleEnv()))
+def run_task(*_):
+	env = TfEnv(normalize(SwimmerEnv()))
 
-policy = GaussianMLPPolicy(
-    name="policy",
-    env_spec=env.spec,
-    # The neural network policy should have two hidden layers, each with 32 hidden units.
-    hidden_sizes=(32, 32)
+	policy = GaussianMLPPolicy(
+		name = 'policy',
+		env_spec=env.spec,
+		# The neural network policy should have two hidden layers, each with 32 hidden units.
+		hidden_sizes=(32, 32),
+	)
+
+	baseline = LinearFeatureBaseline(env_spec=env.spec)
+
+	algo = TRPO(
+		env=env,
+		policy=policy,
+		baseline=baseline,
+		batch_size=4000,
+		max_path_length=500,
+		n_itr=1000,
+		discount=0.99,
+		step_size=0.01,
+		plot = True
+	)
+	algo.train()
+
+run_experiment_lite(
+    run_task,
+    # Number of parallel workers for sampling
+    n_parallel=2,
+    # Only keep the snapshot parameters for the last iteration
+    snapshot_mode="last",
+    # Specifies the seed for the experiment. If this is not provided, a random seed
+    # will be used
+    seed=1,
+    plot=True
 )
-
-baseline = LinearFeatureBaseline(env_spec=env.spec)
-
-algo = TRPO(
-    env=env,
-    policy=policy,
-    baseline=baseline,
-    batch_size=4000,
-    max_path_length=100,
-    n_itr=40,
-    discount=0.99,
-    step_size=0.01,
-    # optimizer=ConjugateGradientOptimizer(hvp_approach=FiniteDifferenceHvp(base_eps=1e-5))
-
-)
-algo.train()
